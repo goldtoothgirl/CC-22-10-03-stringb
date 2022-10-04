e55c15c1e15149cb9777fb23e688d4d66f85ffdb
Carmen Frances <carmenfrances@nem1.net>
2022-10-04 13:51:03 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
+
+  console.log(boolToWord(1), 'yes');
+
+
+
+//   function boolToWord( bool ){
+//     if (bool) {
+//       return 'Yes';
+//     } else {
+//       return 'No';
+//     }
+//   }
